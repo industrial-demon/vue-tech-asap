79766897779caaed4e2d2aa22c76948412a70527
motograter <drew.naumenko@gmail.com>
2025-04-13 21:08:50 +0300

    add base in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     server: {
         port: 3000,
     },
+    base: '/vue-tech-asap',
     resolve: {
         alias: {
             '~': fileURLToPath(new URL('./src', import.meta.url)),
